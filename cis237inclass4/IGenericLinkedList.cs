bc02a24378b9343b2258b52dc682e28ea703ffd4
Jeffrey Martin <animeman1999@google.com>
2016-10-27 16:17:13 -0400

    Makes the linked list generic

---
+++ cis237inclass4/IGenericLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237inclass4
+{
+
+    //This linked list takes in the TYPE called T.
+    //It is declared in the <> portion.
+    //The name can be anything. We choose T, but it
+    //can easily be FOO, or BAR or whatever you want. Standard for C# is "T"
+    //You can even do multiple ones that look lik this:
+    //IGenericeLinkedList<T1,T2>
+    interface IGenericLinkedList<T>
+    {
+        //Since the type we want has come into this interface as 
+        //Type T, we will use T as the type for the Data.
+        //This means that if this linked list is declared with a string
+        //passed in to T, this list will hold strings. If double is sent to T
+        //it will hold doules, and so on.
+        void AddToFront(T GenericData);
+        void AddToBack(T GenericData);
+        T RemoveFromFront();
+        T RemoveFromBack();
+        void Display();
+
+        bool IsEmpty { get; }
+        int Size { get; }
+    }
+}
--- cis237inclass4/Program.cs
+++ cis237inclass4/Program.cs
@@ -19,6 +19,34 @@ namespace cis237inclass4
             myLinkedList.AddToBack(7);
             myLinkedList.AddToBack(8);
             myLinkedList.Display();
+
+            //****************************************
+            //Here is a generic linked list that tores strings
+            IGenericLinkedList<string> myGenericLinkedList = new GenericLinkedList<String>();
+
+
+            //Here is a generic linked list tat stores any class the implements the IComparable interface
+            IGenericLinkedList<IComparable> myComparablelinkedList = new GenericLinkedList<IComparable>();
+
+            //Let's use the string one
+            myGenericLinkedList.AddToBack("Foo");
+            myGenericLinkedList.AddToBack("bar");
+            myGenericLinkedList.AddToBack("Dave");
+            myGenericLinkedList.AddToBack("Barnes");
+
+            myGenericLinkedList.AddToBack("Back 4");
+            myGenericLinkedList.AddToBack("Back 3");
+            myGenericLinkedList.AddToBack("Back 2");
+            myGenericLinkedList.AddToBack("Back 1");
+
+            myGenericLinkedList.Display();
+
+            string frontGuy = myGenericLinkedList.RemoveFromFront();
+
+            Console.WriteLine(frontGuy);
+
+            myGenericLinkedList.Display();
+
         }
     }
 }
